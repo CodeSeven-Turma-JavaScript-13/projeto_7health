6f5ace216ecf5a9df336d96d20c679dae62f6784
code seven <https://github.com/codeseven777>
2026-03-12 19:57:27 -0300

    ✨ New Feature: Construção usuário.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsuarioModule } from './usuario/usuario.module';
+
+import { Usuario } from './usuario/entities/usuario.entity';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+
+
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'jujuba',
+      database: 'db_lojadelivery',
+      entities:[Usuario],
+      synchronize: true,
+      logging: true, // EXIBE O CODIGO SQL NO CONSOLE DO VSCODE // USAR APENAS EM DESENVOLVIMENTO
+    }),
+    UsuarioModule,
+  
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
