7e970707c39c70aed02e3098735017db4c90ed1a
Joao <oliveira110965@gmail.com>
2026-04-16 16:01:47 -0300

    consertando erro

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ async function bootstrap() {
   .setVersion('1.0')
   .addBearerAuth()
   .build();
+  app.enableCors();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/swagger', app, document);
   await app.listen(process.env.PORT ?? 4000);
