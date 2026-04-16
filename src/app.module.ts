7fc07d5b2b426ef7e22cc4fd0641ab2a4d066a3f
Joao <oliveira110965@gmail.com>
2026-04-16 10:36:24 -0300

    deploy do 7health

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,34 +1,26 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsuarioModule } from './usuario/usuario.module';
-
-import { Usuario } from './usuario/entities/usuario.entity';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { Produto } from './produto/entities/produto.entity';
-import { Categoria } from './categoria/entities/categoria.entity';
 import { ProdutoModule } from './produto/produto.module';
 import { CategoriaModule } from './categoria/categoria.module';
+import { ConfigModule } from '@nestjs/config';
+import { ProdService } from './data/services/prod.service';
 
 
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'db_7health',
-      entities:[Usuario, Produto, Categoria],
-      synchronize: true,
-      logging: true, // EXIBE O CODIGO SQL NO CONSOLE DO VSCODE // USAR APENAS EM DESENVOLVIMENTO
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useClass: ProdService,
+      imports: [ConfigModule],
     }),
     UsuarioModule, ProdutoModule, CategoriaModule
-  
+
   ],
   controllers: [AppController],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
